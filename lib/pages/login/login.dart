56e01a453088f76c6d74a7cb510a476cbb2fa232
eaTong <eatongchou@gmail.com>
2020-03-31 15:16:21 +0800

    login page

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -9,23 +9,77 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String account;
+  String password;
+
+  _validateAndSubmit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print(account);
+      print(password);
+    } else {
+      _formKey.currentState.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
+          child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    child: Text(
-                      'Login page',
-                      style: TextStyle(
-                          fontSize: 40, decoration: null, color: Colors.blue),
-                    )),
-              ]),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'account should not be empty';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  account = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'account',
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'account should not be empty';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  password = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'password',
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: _validateAndSubmit,
+                      child: Text('submit'),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      )),
     );
   }
 }
